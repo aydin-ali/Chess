0f9b31358b7e0e9e553f42dfca075d8163d43f66
aydin-ali <aydinali.kachra@gmail.com>
2022-12-01 23:38:07 -0500

    valid move

--- src/pieces/bishop.cc
+++ src/pieces/bishop.cc
@@ -1,14 +1,108 @@
 #include "bishop.h"
 using namespace std;
 
-Bishop::Bishop(const string &colour):
-    Piece{colour, 'b'}{}
+Bishop::Bishop(const string &colour, int row, int col):
+    Piece{colour, 'b', row, col} {}
 
 bool Bishop::validMove(Move move, vector<vector<Piece*>> board) {
     return true;
 }
 
-void Bishop::updatePossibleMoves(Move move, vector<vector<Piece*>>) {
+void Bishop::updatePossibleMoves(vector<vector<Piece*>> board) {
+    int row;
+    int col;
+    int incr;
+    Position p{0 ,0};
+
+    row = posn.getRow();
+    col = posn.getCol();
+    incr = 1;
+    p = {row - incr, col - incr};
+
+    while(p.validPosn()) {
+        if (board[p.getRow()][p.getCol()] == nullptr) {
+            possibleMoves.emplace_back(p);
+            ++incr;
+            p = {row - incr, col - incr};
+        } else if (board[p.getRow()][p.getCol()]->getColour() != colour) {
+            possibleMoves.emplace_back(p);
+            break;
+        } else {
+            break;
+        }
+    }
+
+    row = posn.getRow();
+    col = posn.getCol();
+    incr = 1;
+    p = {row - incr, col - incr};
+
+    while(p.validPosn()) {
+        if (board[p.getRow()][p.getCol()] == nullptr) {
+            possibleMoves.emplace_back(p);
+            ++incr;
+            p = {row - incr, col - incr};
+        } else if (board[p.getRow()][p.getCol()]->getColour() != colour) {
+            possibleMoves.emplace_back(p);
+            break;
+        } else {
+            break;
+        }
+    }
+
+    row = posn.getRow();
+    col = posn.getCol();
+    incr = 1;
+    p = {row - incr, col + incr};
+
+    while(p.validPosn()) {
+        if (board[p.getRow()][p.getCol()] == nullptr) {
+            possibleMoves.emplace_back(p);
+            ++incr;
+            p = {row - incr, col + incr};
+        } else if (board[p.getRow()][p.getCol()]->getColour() != colour) {
+            possibleMoves.emplace_back(p);
+            break;
+        } else {
+            break;
+        }
+    }
+
+    row = posn.getRow();
+    col = posn.getCol();
+    incr = 1;
+    p = {row + incr, col + incr};
+
+    while(p.validPosn()) {
+        if (board[p.getRow()][p.getCol()] == nullptr) {
+            possibleMoves.emplace_back(p);
+            ++incr;
+            p = {row + incr, col + incr};
+        } else if (board[p.getRow()][p.getCol()]->getColour() != colour) {
+            possibleMoves.emplace_back(p);
+            break;
+        } else {
+            break;
+        }
+    }
+
+    row = posn.getRow();
+    col = posn.getCol();
+    incr = 1;
+    p = {row + incr, col + incr};
+
+    while(p.validPosn()) {
+        if (board[p.getRow()][p.getCol()] == nullptr) {
+            possibleMoves.emplace_back(p);
+            ++incr;
+            p = {row + incr, col + incr};
+        } else if (board[p.getRow()][p.getCol()]->getColour() != colour) {
+            possibleMoves.emplace_back(p);
+            break;
+        } else {
+            break;
+        }
+    }
     
 }
 
