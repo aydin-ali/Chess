2614fba13ac72faf5f0860b0b07a716b0097ff3e
AndyyZhu <andyzhu200@gmail.com>
2022-12-02 00:08:09 -0500

    rook implementation not tested

--- src/pieces/rook.cc
+++ src/pieces/rook.cc
@@ -8,6 +8,72 @@ bool Rook::validMove(Move move, vector<vector<Piece*>> board) {
     return true;
 }
 
-void Rook::updatePossibleMoves(Move move, vector<vector<Piece*>>) {
-    
+void Rook::updatePossibleMoves(Move move, vector<vector<Piece*>> board) {
+    int row;
+    int col;
+    Position p = {0, 0};
+
+    row = posn.getRow();
+    col = posn.getCol();
+    // Moving the Rook left
+    while(p.validPosn()){
+        --col;
+        p = {row, col};
+        if(board[row][col] == nullptr){
+            possibleMoves.emplace_back(p);
+        } else if(board[row][col]->getColour() != colour){
+            possibleMoves.emplace_back(p);
+            break;
+        } else {
+            break;
+        }
+    }
+
+    row = posn.getRow();
+    col = posn.getCol();
+    // Moving the Rook right
+    while(p.validPosn()){
+        ++col;
+        p = {row, col};
+        if(board[row][col] == nullptr){
+            possibleMoves.emplace_back(p);
+        } else if(board[row][col]->getColour() != colour){
+            possibleMoves.emplace_back(p);
+            break;
+        } else {
+            break;
+        }
+    }
+
+    row = posn.getRow();
+    col = posn.getCol();
+    // Moving the Rook up
+    while(p.validPosn()){
+        ++row;
+        p = {row, col};
+        if(board[row][col] == nullptr){
+            possibleMoves.emplace_back(p);
+        } else if(board[row][col]->getColour() != colour){
+            possibleMoves.emplace_back(p);
+            break;
+        } else {
+            break;
+        }
+    }
+
+    row = posn.getRow();
+    col = posn.getCol();
+    // Moving the Rook down
+    while(p.validPosn()){
+        --row;
+        p = {row, col};
+        if(board[row][col] == nullptr){
+            possibleMoves.emplace_back(p);
+        } else if(board[row][col]->getColour() != colour){
+            possibleMoves.emplace_back(p);
+            break;
+        } else {
+            break;
+        }
+    }
 }
