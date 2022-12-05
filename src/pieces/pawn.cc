6a542cc453fb38ad46b6fdb2e4aa73ab12adf1e9
aydin-ali <aydinali.kachra@gmail.com>
2022-12-04 19:48:12 -0500

    fixing overrides

--- src/pieces/pawn.cc
+++ src/pieces/pawn.cc
@@ -1,4 +1,6 @@
 #include "pawn.h"
+#include "../board/board.h"
+#include <iostream>
 using namespace std;
 
 Pawn::Pawn(const string colour, int row, int col):
@@ -9,7 +11,6 @@ bool Pawn::validMove(Move move, vector<vector<Piece*>> board) {
 
     //call a function that fills a vector w/ possible moves    
     //all the checking should happen here
-    updatePossibleMoves(board);
 
     //check if the given move is within the possibleMoves vector
     for (auto it = possibleMoves.begin(); it != possibleMoves.end(); ++it) {
@@ -32,7 +33,7 @@ bool Pawn::validMove(Move move, vector<vector<Piece*>> board) {
 
 //side note: pawn promotion and poisson stuff
 
-void Pawn::updatePossibleMoves(vector<vector<Piece*>> board) {
+void Pawn::updatePossibleMoves(vector<vector<Piece*>> board, Board &b) {
 
     int rowIncrement;
     if (colour == "white") {
@@ -61,23 +62,28 @@ void Pawn::updatePossibleMoves(vector<vector<Piece*>> board) {
         if (!moved) { 
             //check 1 square in front
             if (board[p.getRow()][p.getCol()] == nullptr) {
-                possibleMoves.emplace_back(p);
-                
-                //if that's a valid move, check 2 squares in front --------
-                row = posn.getRow() + (rowIncrement * 2);
-                col = posn.getCol();
-                p = {row, col};
-                if (p.positionWithinBounds()) { //recheck if the new position is valid because we changed it
-                    if (board[p.getRow()][p.getCol()] == nullptr) {
-                        possibleMoves.emplace_back(p);
+                if (moveAllowed(b, p)) {
+                    possibleMoves.emplace_back(p);
+                    //if that's a valid move, check 2 squares in front --------
+                    row = posn.getRow() + (rowIncrement * 2);
+                    col = posn.getCol();
+                    p = {row, col};
+                    if (p.positionWithinBounds()) { //recheck if the new position is valid because we changed it
+                        if (board[p.getRow()][p.getCol()] == nullptr) {
+                            if (moveAllowed(b, p)) {
+                                possibleMoves.emplace_back(p);
+                            }
+                        }
                     }
                 }
-                //---------------------------------------------------------
+                    //---------------------------------------------------------
             }
         } else {
             //check only one square in front
             if (board[p.getRow()][p.getCol()] == nullptr) {
-                possibleMoves.emplace_back(p);
+                if (moveAllowed(b, p)) {
+                    possibleMoves.emplace_back(p);
+                }
             }
         }
     }
@@ -89,7 +95,9 @@ void Pawn::updatePossibleMoves(vector<vector<Piece*>> board) {
     p = {row, col};
     if (p.positionWithinBounds()) {
         if (board[p.getRow()][p.getCol()] != nullptr && board[p.getRow()][p.getCol()]->getColour() != colour){
-            possibleMoves.emplace_back(p);
+            if (moveAllowed(b, p)) {
+                possibleMoves.emplace_back(p);
+            }
         }
     }
     //diagonal left
@@ -98,15 +106,13 @@ void Pawn::updatePossibleMoves(vector<vector<Piece*>> board) {
     p = {row, col};
     if (p.positionWithinBounds()) {
         if (board[p.getRow()][p.getCol()] != nullptr && board[p.getRow()][p.getCol()]->getColour() != colour){
-            possibleMoves.emplace_back(p);
+            if (moveAllowed(b, p)) {
+                possibleMoves.emplace_back(p);
+            }
         }
     }
-
 }
 
 //if the pawn moving leads to its own king being checked (LATER)
 
-
 //side note: to keep track of wins/losses in player, write a visitor?
-
-
